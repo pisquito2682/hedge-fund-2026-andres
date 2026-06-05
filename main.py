d7556ba55934f7854da2645a84a2129b125e61a3
pisquito2682 <alvarezandrespisco@gmail.com>
2026-06-05 12:04:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,18 @@ import os
 
 app = FastAPI()
 
-API_KEY = os.getenv("ODDS_API_KEY")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-
-
-def send_telegram(message):
-
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-    requests.post(url, json={
-        "chat_id": CHAT_ID,
-        "text": message
-    })
 
 
 @app.get("/")
 def home():
 
-    send_telegram("🔥 BOT FUNCIONANDO CORRECTAMENTE")
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": "🔥 BOT FUNCIONANDO CORRECTAMENTE"
+    })
 
     return {"status": "ok"}
